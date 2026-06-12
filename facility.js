3e73c84d6d9e6db71b4ebc4af6847dbfdef76a10
Ctech <osonduchikamso1999@gmail.com>
2026-06-12 15:01:58 +0100

    Update NINA HOTELS LTD website

--- facility.js
+++ facility.js
@@ -1,8 +1,52 @@
-    const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
-    const dropdownmenu=document.querySelector('.dropdown-menu')
+    // const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+    // const dropdownmenu=document.querySelector('.dropdown-menu')
 
 
 
-    const dropdownList = [...dropdownElementList].map(dropdownToggleEl => 
-    new bootstrap.Dropdown(dropdownToggleEl))
+    // const dropdownList = [...dropdownElementList].map(dropdownToggleEl => 
+    // new bootstrap.Dropdown(dropdownToggleEl))
 
+// const buttons= document.querySelectorAll('.button');
+
+
+// buttons.forEach(button => {
+//   button.addEventListener('click', ()=>{
+
+//     alert('active');
+//   });
+   
+    
+// });
+
+//  console.log(buttons);
+
+
+//  ===========selecting buttons====
+// const Loungebtn = document.querySelector('.Loungebtn');
+// const OutdoorBarbtn = document.querySelector('.EventHallbtn');
+// const EventHallbtn = document.querySelector('.EventHallbtn');
+// const submitbutton = document.querySelector('.submitbutton');
+
+
+
+//    OutdoorBarbtn.addEventListener('click', () =>{
+//     alert('active');
+//   });
+//    console.log(OutdoorBarbtn);
+
+
+//    OutdoorBarbtn.addEventListener('click', () =>{
+//     alert('active');
+//   });
+//    console.log(OutdoorBarbtn);
+
+
+//     EventHallbtn.addEventListener('click', () =>{
+//     alert('active');
+//   });
+//    console.log(EventHallbtn);
+
+//     submitbutton.addEventListener('click', () =>{
+//     alert('active');
+//   });
+//    console.log(submitbutton);
